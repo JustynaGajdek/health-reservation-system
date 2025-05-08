938f109ac1375b476b9bc174d66aeac2dc9ba097
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2025-05-08 18:15:48 +0200

    feat: add AppointmentRequestDto with preferredDateTime and type

--- backend/src/main/java/com/justynagajdek/healthreservationsystem/controller/AppointmentController.java
+++ backend/src/main/java/com/justynagajdek/healthreservationsystem/controller/AppointmentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+public class AppointmentController {
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/hello")
     public String helloWorld() {
