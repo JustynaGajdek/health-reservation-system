cc5485f4db85d88c0f62039d0e9d91d5da7f01eb
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2025-05-10 10:03:46 +0200

    feat: added @ActiveProfiles("test") to all SpringBootTests

--- backend/src/test/java/com/justynagajdek/healthreservationsystem/UserMapperTest.java
+++ backend/src/test/java/com/justynagajdek/healthreservationsystem/UserMapperTest.java
@@ -8,9 +8,10 @@ import com.justynagajdek.healthreservationsystem.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@ActiveProfiles("test")
 @SpringBootTest
 class UserMapperTest {
 
