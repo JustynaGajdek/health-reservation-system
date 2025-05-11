b5d571950566639db343f506f21baa5401bbb64f
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2025-05-11 10:40:36 +0200

    refactor(admin): return UserDto instead of UserEntity and apply UserMapper

---
+++ backend/src/main/java/com/justynagajdek/healthreservationsystem/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.justynagajdek.healthreservationsystem.dto;
+
+import com.justynagajdek.healthreservationsystem.enums.AccountStatus;
+import com.justynagajdek.healthreservationsystem.enums.Role;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private AccountStatus status;
+}
